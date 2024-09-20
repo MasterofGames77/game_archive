9c05a44479e9e280e10e926ed6ca342c64cb5952
MasterofGames77 <mgambardella16@gmail.com>
2024-09-20 12:34:14 -0400

    updated connection function

--- backend/server.js
+++ backend/server.js
@@ -40,8 +40,10 @@ async function startServer() {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
-            ssl: { rejectUnauthorized: false }
-        });
+            ssl: {
+              rejectUnauthorized: false
+            }
+          });          
 
         console.log('Connected to MySQL Server!');
 
